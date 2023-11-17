57c8676b3cdf12f57d9a4bcc6bf4a8496824dae7
kusa <th.kusa87@gmail.com>
2023-11-17 22:38:39 +0900

    add and sub

--- mycc.c
+++ mycc.c
@@ -3,14 +3,32 @@
 
 int main(int argc, char **argv) {
     if (argc != 2) {
-        fprintf(stderr, "Args count\n");
+        fprintf(stderr, "Error: args count\n");
         return 1;
     } 
+
+    char *p = argv[1];
 
     printf(".intel_syntax noprefix\n");
     printf(".globl main\n");
     printf("main:\n");
-    printf("  mov rax, %d\n", atoi(argv[1]));
+    printf("  mov rax, %ld\n", strtol(p, &p, 10));
+
+    while (*p) {
+        if (*p == '+') {
+            p++;
+            printf("  add rax, %ld\n", strtol(p, &p, 10));
+            continue;
+        }
+        if (*p == '-') {
+            p++;
+            printf("  sub rax, %ld\n", strtol(p, &p, 10));
+            continue;
+        }
+
+        fprintf(stderr, "Error: unexpected charactor '%c'\n", *p);
+        return 1;
+    }
     printf("  ret\n");
     return 0;
 }
